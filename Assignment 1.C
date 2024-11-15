4be380fcf4ea3ce45f6129a511facc8e314a6e5d
24csu009 <24csu009@ncuindia.edu>
2024-11-15 15:43:25 +0530

    Assignment 1.C

--- Assignment 1.C
+++ Assignment 1.C
@@ -37,7 +37,7 @@ int main() {
     printf("Enter two integers: ");
     scanf("%d %d", &num1, &num2);
     for(i = 1; i <= num1 && i <= num2; ++i) {
-        // If i is a divisor of both numbers, update hcf
+        
         if(num1 % i == 0 && num2 % i == 0) {
             hcf = i;
         }
